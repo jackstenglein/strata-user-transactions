655a69f0725c90369c5196188e655e91896faf66
Jack Stenglein <jackstenglein@utexas.edu>
2019-12-10 16:11:46 -0600

    Add unlink abort test

--- libfs/tests/transactions/transactions_all_abort.c
+++ libfs/tests/transactions/transactions_all_abort.c
@@ -16,11 +16,11 @@
 #include <mlfs/mlfs_interface.h>
 
 #define BUF_SIZE 4096
-#define TEST_DIR "/mlfs/transactions-create"
+#define TEST_DIR "/mlfs/transactions-abort"
 
 void test_open_with_create(void) {
     // Try to open without O_CREAT, should fail
-    int fd1 = open(TEST_DIR "/test1", O_RDWR, 0600);
+    int fd1 = open(TEST_DIR "/open", O_RDWR, 0600);
     if (fd1 >= 0) {
         printf("Test Failed: file already exists\n");
         return;
@@ -30,7 +30,7 @@ void test_open_with_create(void) {
     start_log_usr_tx();
 
     // Open the file and create it
-    fd1 = open(TEST_DIR "/test1", O_RDWR|O_CREAT, 0600);
+    fd1 = open(TEST_DIR "/open", O_RDWR|O_CREAT, 0600);
     if (fd1 < 0) {
         perror("Test Failed: Unable to create file during transaction");
         abort_log_usr_tx();
@@ -41,7 +41,7 @@ void test_open_with_create(void) {
     abort_log_usr_tx();
 
     // Try to open without O_CREAT, should still fail
-    fd1 = open(TEST_DIR "/test1", O_RDWR, 0600);
+    fd1 = open(TEST_DIR "/open", O_RDWR, 0600);
     if (fd1 >= 0) {
         printf("Test Failed: file exists after abort\n");
         return;
@@ -124,6 +124,37 @@ void test_rmdir(void) {
     closedir(dir);
 }
 
+void test_unlink(void) {
+    // Open with O_CREAT to ensure the file exists
+    int fd1 = open(TEST_DIR "/unlink", O_RDWR|O_CREAT, 0600);
+    if (fd1 < 0) {
+        perror("Test Failed: unable to open starting file\n");
+        return;
+    }
+    close(fd1);
+
+    // Start transaction
+    start_log_usr_tx();
+
+    int err = unlink(TEST_DIR "/unlink");
+    if (err < 0) {
+        perror("Test Failed: Unable to unlink file during transaction");
+        abort_log_usr_tx();
+        return;
+    }
+
+    // Abort transaction
+    abort_log_usr_tx();
+
+    // Open without O_CREAT to verify it still exists
+    fd1 = open(TEST_DIR "/unlink", O_RDWR, 0600);
+    if (fd1 < 0) {
+        perror("Test Failed: Unable to open file after abort\n");
+        return;
+    }
+    close(fd1);
+}
+
 int main(int argc, char ** argv)
 {
 	init_fs();
@@ -142,6 +173,9 @@ int main(int argc, char ** argv)
 
     printf("\nTesting rmdir\n");
     test_rmdir();
+
+    printf("\nTesting unlink\n");
+    test_unlink();
     
 	pause();
     return 0;
