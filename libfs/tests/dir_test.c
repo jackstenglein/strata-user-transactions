b8492245b49d7904cc090c720746b189221bf710
Jack Stenglein <jackstenglein@utexas.edu>
2019-12-10 13:25:12 -0600

    Update dir_test

--- libfs/tests/dir_test.c
+++ libfs/tests/dir_test.c
@@ -10,6 +10,22 @@
 #define TESTDIR "/mlfs/test_dir"
 #define N_FILES 1000
 
+void printStats(struct stat* file_stats) {
+    printf("\t\tDevice: %d\n", file_stats->st_dev);
+	printf("\t\tInode: %d\n", file_stats->st_ino);
+	printf("\t\tMode: %d\n", file_stats->st_mode);
+	printf("\t\tHard Links: %d\n", file_stats->st_nlink);
+	printf("\t\tUser ID: %d\n", file_stats->st_uid);
+	printf("\t\tGroup ID: %d\n", file_stats->st_gid);
+	printf("\t\tRdev: %d\n", file_stats->st_rdev);
+	printf("\t\tSize: %d\n", file_stats->st_size);
+	printf("\t\tBlock size: %d\n", file_stats->st_blksize);
+	printf("\t\tBlocks: %d\n", file_stats->st_blocks);
+	printf("\t\tAccess time: %lld.%.9ld\n", (long long)file_stats->st_atim.tv_sec, file_stats->st_atim.tv_nsec);
+	printf("\t\tModified time: %lld.%.9ld\n", (long long)file_stats->st_mtim.tv_sec, file_stats->st_mtim.tv_nsec);
+	printf("\t\tChange time: %lld.%.9ld\n", (long long)file_stats->st_ctim.tv_sec, file_stats->st_ctim.tv_nsec);
+}
+
 int main(int argc, char ** argv)
 {
     struct stat buf;
@@ -71,7 +87,26 @@ int main(int argc, char ** argv)
 		exit(1);
 	}
 
+    struct stat file_stats;
+    if ((ret = stat(TESTDIR "/files/file2", &file_stats)) < 0) {
+        perror("stat");
+        exit(1);
+    }
+    printf("file stats before digesting: \n");
+    printStats(&file_stats);
+
 	shutdown_fs();
+
+    init_fs();
+
+    if ((ret = stat(TESTDIR "/files/file2", &file_stats)) < 0) {
+        perror("stat");
+        exit(1);
+    }
+    printf("file stats after digesting: \n");
+    printStats(&file_stats);
+
+    shutdown_fs();
 
     return 0;
 }
